414e44b245bd8867ee1553cef8cc5946754d0698
remimimimimi <remimimimimi@protonmail.com>
2024-07-22 16:27:08 +0300

    Add handlers for editable labels

--- src/peer.rs
+++ src/peer.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use gtk::prelude::*;
 use relm4::prelude::*;
 
@@ -16,6 +18,18 @@ impl PeerComp {
     }
 }
 
+pub enum PeerInput {
+    Modify(Box<dyn FnOnce(&mut Peer)>)
+}
+
+impl fmt::Debug for PeerInput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PeerInput::Modify(_) => f.write_str("SetCurrentTunnel(<fn>)"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum PeerOutput {
     Remove(DynamicIndex),
@@ -24,7 +38,7 @@ pub enum PeerOutput {
 #[relm4::factory(pub)]
 impl FactoryComponent for PeerComp {
     type Init = Peer;
-    type Input = ();
+    type Input = PeerInput;
     type Output = PeerOutput;
     type CommandOutput = ();
     type ParentWidget = gtk::Box;
@@ -57,8 +71,13 @@ impl FactoryComponent for PeerComp {
                     set_halign: gtk::Align::Start,
                 },
                 attach[1, 0, 1, 1] = &gtk::EditableLabel {
-                    #[watch]
                     set_text: &self.peer.name.clone().unwrap_or("unknown".into()),
+                    connect_changed[sender] => move |l| {
+                        let new: String = l.text().trim().into();
+                        sender.input(Self::Input::Modify(Box::new(|p| {
+                            p.name = (new != "unknown").then_some(new);
+                        })));
+                    },
                 },
 
                 attach[0, 1, 1, 1] = &gtk::Label {
@@ -66,8 +85,13 @@ impl FactoryComponent for PeerComp {
                     set_halign: gtk::Align::Start,
                 },
                 attach[1, 1, 1, 1] = &gtk::EditableLabel {
-                    #[watch]
                     set_text: &self.peer.allowed_ips.clone().unwrap_or("unknown".into()),
+                    connect_changed[sender] => move |l| {
+                        let new: String = l.text().trim().into();
+                        sender.input(Self::Input::Modify(Box::new(|p| {
+                            p.allowed_ips = (new != "unknown").then_some(new);
+                        })));
+                    },
                 },
 
                 attach[0, 2, 1, 1] = &gtk::Label {
@@ -75,8 +99,13 @@ impl FactoryComponent for PeerComp {
                     set_halign: gtk::Align::Start,
                 },
                 attach[1, 2, 1, 1] = &gtk::EditableLabel {
-                    #[watch]
                     set_text: &self.peer.endpoint.clone().unwrap_or("unknown".into()),
+                    connect_changed[sender] => move |l| {
+                        let new: String = l.text().trim().into();
+                        sender.input(Self::Input::Modify(Box::new(|p| {
+                            p.endpoint = (new != "unknown").then_some(new);
+                        })));
+                    },
                 },
 
                 attach[0, 3, 1, 1] = &gtk::Label {
@@ -84,8 +113,13 @@ impl FactoryComponent for PeerComp {
                     set_halign: gtk::Align::Start,
                 },
                 attach[1, 3, 1, 1] = &gtk::EditableLabel {
-                    #[watch]
                     set_text: &self.peer.public_key.clone().unwrap_or("unknown".into()),
+                    connect_changed[sender] => move |l| {
+                        let new: String = l.text().trim().into();
+                        sender.input(Self::Input::Modify(Box::new(|p| {
+                            p.public_key = (new != "unknown").then_some(new);
+                        })));
+                    },
                 },
 
                 attach[0, 4, 1, 1] = &gtk::Label {
@@ -93,8 +127,13 @@ impl FactoryComponent for PeerComp {
                     set_halign: gtk::Align::Start,
                 },
                 attach[1, 4, 1, 1] = &gtk::EditableLabel {
-                    #[watch]
                     set_text: &self.peer.persistent_keepalive.clone().unwrap_or("unknown".into()),
+                    connect_changed[sender] => move |l| {
+                        let new: String = l.text().trim().into();
+                        sender.input(Self::Input::Modify(Box::new(|p| {
+                            p.persistent_keepalive = (new != "unknown").then_some(new);
+                        })));
+                    },
                 },
             }
         }
@@ -102,5 +141,14 @@ impl FactoryComponent for PeerComp {
 
     fn init_model(peer_config: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
         Self::new(peer_config)
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: relm4::FactorySender<Self>) {
+        match msg {
+            Self::Input::Modify(f) => {
+                f(&mut self.peer);
+                dbg!(&self.peer);
+            },
+        }
     }
 }
