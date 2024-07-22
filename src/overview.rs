0124257cd4aa064f55a5d1801689ecd534265b3f
remimimimimi <remimimimimi@protonmail.com>
2024-07-22 21:04:43 +0300

    Render Interface info

--- src/overview.rs
+++ src/overview.rs
@@ -22,11 +22,28 @@ impl OverviewModel {
 }
 
 #[derive(Debug)]
+pub enum InterfaceSetKind {
+    Name,
+    Address,
+    ListenPort,
+    PrivateKey,
+    Dns,
+    Table,
+    Mtu,
+    PreUp,
+    PostUp,
+    PreDown,
+    PostDown,
+}
+
+#[derive(Debug)]
 pub enum OverviewInput {
     CollectTunnel,
     ShowConfig(Box<WireguardConfig>),
     RemovePeer(DynamicIndex),
     AddPeer,
+    SetInterface(InterfaceSetKind, Option<String>),
+
 }
 
 #[derive(Debug)]
@@ -46,7 +63,191 @@ impl SimpleComponent for OverviewModel {
             set_orientation: gtk::Orientation::Vertical,
 
             gtk::Frame::new(Some("Interface:")) {
+                gtk::Grid {
+                    set_row_spacing: 5,
+                    set_column_spacing: 5,
+                    set_margin_all: 5,
 
+                    attach[0, 0, 1, 1] = &gtk::Label {
+                        set_label: "# Name:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "name"]
+                    attach[1, 0, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: match model.interface.name {
+                            Some(ref s) => s.as_str(),
+                            None => "unknown",
+                        },
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::Name, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 1, 1, 1] = &gtk::Label {
+                        set_label: "Address:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "address"]
+                    attach[1, 1, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.address.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::Address, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 2, 1, 1] = &gtk::Label {
+                        set_label: "ListenPort:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "listen_port"]
+                    attach[1, 2, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.listen_port.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::ListenPort, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    // TODO: Just show omitted
+                    attach[0, 3, 1, 1] = &gtk::Label {
+                        set_label: "PrivateKey:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "private_key"]
+                    attach[1, 3, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.private_key.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::PrivateKey, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 4, 1, 1] = &gtk::Label {
+                        set_label: "DNS:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "dns"]
+                    attach[1, 4, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.dns.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::Dns, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 5, 1, 1] = &gtk::Label {
+                        set_label: "Table:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "table"]
+                    attach[1, 5, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.table.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::Table, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 6, 1, 1] = &gtk::Label {
+                        set_label: "MTU:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "mtu"]
+                    attach[1, 6, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.mtu.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::Mtu, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 7, 1, 1] = &gtk::Label {
+                        set_label: "PreUp:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "pre_up"]
+                    attach[1, 7, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.pre_up.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::PreUp, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 8, 1, 1] = &gtk::Label {
+                        set_label: "PostUp:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "post_up"]
+                    attach[1, 8, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.post_up.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::PostUp, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 9, 1, 1] = &gtk::Label {
+                        set_label: "PreDown:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "pre_down"]
+                    attach[1, 9, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.pre_down.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::PreDown, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+
+                    attach[0, 10, 1, 1] = &gtk::Label {
+                        set_label: "PostDown:",
+                        set_halign: gtk::Align::Start,
+                    },
+                    #[name = "post_down"]
+                    attach[1, 10, 1, 1] = &gtk::EditableLabel {
+                        #[watch]
+                        set_text: &model.interface.post_down.clone().unwrap_or("unknown".into()),
+                        connect_editing_notify[sender] => move |l| {
+                            if !l.is_editing() {
+                                let new: String = l.text().trim().into();
+                                sender.input(Self::Input::SetInterface(InterfaceSetKind::PostDown, (new != "unknown").then_some(new)));
+                            }
+                        },
+                    },
+                }
             },
 
             append: model.peers.widget()
@@ -96,6 +297,21 @@ impl SimpleComponent for OverviewModel {
             Self::Input::AddPeer => {
                 let mut peers = self.peers.guard();
                 peers.push_back(Peer::default());
+            },
+            Self::Input::SetInterface(kind, value) => {
+                match kind {
+                    InterfaceSetKind::Name => self.interface.name = dbg!(value),
+                    InterfaceSetKind::Address => self.interface.address = value,
+                    InterfaceSetKind::ListenPort => self.interface.listen_port = value,
+                    InterfaceSetKind::PrivateKey => self.interface.private_key = value,
+                    InterfaceSetKind::Dns => self.interface.dns = value,
+                    InterfaceSetKind::Table => self.interface.table = value,
+                    InterfaceSetKind::Mtu => self.interface.mtu = value,
+                    InterfaceSetKind::PreUp => self.interface.pre_up = value,
+                    InterfaceSetKind::PostUp => self.interface.post_up = value,
+                    InterfaceSetKind::PreDown => self.interface.pre_down = value,
+                    InterfaceSetKind::PostDown => self.interface.post_down = value,
+                }
             }
         }
     }
